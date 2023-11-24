d4b2a59ff78de9cbe5eed7c608cc700b34a9f7a8
Willem Johan Van Der Vorst <wvan-der@c2r5p4.42vienna.com>
2023-11-24 17:18:31 +0100

    fixed header

--- minishel.h
+++ minishel.h
@@ -6,40 +6,37 @@
 
 typedef struct s_vars
 {
-	char *key;
-	char *value;
-	s_vars *next;
-} t_vars;
+	char				*key;
+	char 				*value;
+	struct s_vars 		*next;
+}	t_vars;
 
 typedef struct s_redirect
 {
-	char *redirect;
-	s_redirect *next;
+	char 				*redirect;
+	struct s_redirect	*next;
 }	t_redirect;
 
 typedef struct s_cmd
 {
-	char		*path;
-	char		**args;
-	t_redirect 	*redirect_lst;
-	s_cmd		*next;
+	char				*path;
+	char				**args;
+	t_redirect			*redirect_lst;
+	struct s_cmd		*next;
 
 }	t_cmd;
 
 typedef struct s_shell
 {
-	t_cmd	cmd_lst;
-	int last_exit_code;
-	t_vars	vars;
-
+	t_cmd		cmd_lst;
+	int			last_exit_code;
+	t_vars		vars;
 }	t_shell;
 
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdlib.h>
-
-char	**ft_split(char *str);
-
+#include <unistd.h>
 
 #endif
