0183143b52f19a1e39815abbab986a62d330f1b9
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-11-27 16:49:43 +0100

    adding folder, 42 headers and norminet headaer

---
+++ headers/minishel.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishel.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fseles <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 16:43:41 by fseles            #+#    #+#             */
+/*   Updated: 2023/11/27 16:43:45 by fseles           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHEL_H
+# define MINISHEL_H
+# define NAME "minishel: "
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_vars
+{
+	char			*key;
+	char			*value;
+	struct s_vars	*next;
+}	t_vars;
+
+typedef struct s_redirect
+{
+	char				*redirect;
+	struct s_redirect	*next;
+}	t_redirect;
+
+typedef struct s_cmd
+{
+	char			*path;
+	char			**args;
+	t_redirect		*redirect_lst;
+	struct s_cmd	*next;
+
+}	t_cmd;
+
+typedef struct s_shell
+{
+	t_cmd	cmd_lst;
+	int		last_exit_code;
+	t_vars	vars;
+}	t_shell;
+
+#endif
