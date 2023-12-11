786ef5a77abafd1b6a498a9eb6e202f14602e7f6
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-12-11 20:58:48 +0100

    soory arthur and artem for wasting so much of your time on stupid file leaks . FOR FUTUTRE FIIP CLOSE STDOUT AND STDIN

--- headers/execution.h
+++ headers/execution.h
@@ -19,6 +19,7 @@ void clear_pipe_array(int ***pipe_arr);
 int input_redirect(t_redirect *input, int *fd);
 int is_cmd_builtin(t_cmd *cmd);
 int make_pipes(int ***pipe_arr, int number_of_pipes);
+int output_redirect(t_redirect *output, int *fd);
 int set_number_of_commands(int *noc, t_cmd *cmd);
 
 #endif
