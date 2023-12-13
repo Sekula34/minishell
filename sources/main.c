ce647d23e6576b79d0d73d60275aa00c1fc9cf24
Filip Seles <fseles@c2r5p7.42vienna.com>
2023-12-13 16:52:53 +0100

    main.c

--- sources/main.c
+++ sources/main.c
@@ -18,24 +18,19 @@ int	main(int argc, char **argv, char **envp)
 	{
 
 	}
-	char *command[] = {"wc", "-l", NULL}; // Replace "file.txt" with the name of the file you want to count lines for
+	//char *command[] = {"wc", "-l", NULL}; // Replace "file.txt" with the name of the file you want to count lines for
 
-	int input;
-	int output;
-	t_redirect firs_red;
-	firs_red.type = 'i';
-	firs_red.file_name = "input.txt";
-	t_redirect second_red;
-	second_red.type ='o';
-	second_red.file_name ="output.txt";
-	firs_red.next = &second_red;
-	second_red.next = NULL;
-	if(redirect_handler(&firs_red, &input, &output) != 0)
-		return(EXIT_FAILURE);
-	printf("Ajde printaj sad ovo u picku materinu\n");
-    if (execve("/usr/bin/wc", command, NULL) == -1) {
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
-	return(EXIT_SUCCESS);
+	t_shell shell;
+
+	if(shell_init(&shell, envp) == 1)
+	{
+		clear_list_env(&shell.head_env);
+		clear_list_env(&shell.head_ex);
+		return (EXIT_FAILURE);
+
+	}
+	export(NULL, &shell.head_ex, &shell.head_env);
+	clear_list_env(&shell.head_env);
+	clear_list_env(&shell.head_ex);
+	return (0);
 } 
