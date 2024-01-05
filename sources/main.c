4a395a36c42836a56a74a265193b9bfb578ad683
Willem Johan Van Der Vorst <wvan-der@c2r5p4.42vienna.com>
2024-01-05 17:40:58 +0100

    added my new stuff

--- sources/main.c
+++ sources/main.c
@@ -15,25 +15,21 @@ int main(int argc, char **argv, char **envp)
 	int i = 0;
 	while(1)
 	{
-/* 		line = readline("minishell: ");
+		line = readline("minishell: ");
 		if (!line)
-			exit(0); */
+			exit(0);
 		line = "echo hello";
 		if (parsing(&shell, line) == 0)
 		{
 			shexit(&shell, 1);
 		}
-
-
-		
-		
 		puts("after parsing");
 		puts("");
 		shell.first_cmd_copy = shell.cmd_lst;
 		if(heredoc_parent_prepare(shell.cmd_lst) != 0)
 			shexit(&shell, 1);
 		if(execute_all_cmds(&shell) != 0)
-			shexit(&shell, 1); */
+			shexit(&shell, 1);
 		clear_all_commands(&shell.first_cmd_copy);
 		shell.first_cmd_copy = NULL;
 		i++;
