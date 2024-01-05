8f7eb96426892f8f39899a16078e793b0e2cd192
filip <filip.seles@gmail.com>
2024-01-05 17:49:50 +0100

    echo hello fixed

--- sources/main.c
+++ sources/main.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv, char **envp)
 		line = readline("minishell: ");
 		if (!line)
 			exit(0);
-		line = "echo hello";
+		///line = "echo hello";
 		if (parsing(&shell, line) == 0)
 		{
 			shexit(&shell, 1);
