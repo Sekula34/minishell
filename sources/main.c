5ee4121fe07f311934427431a91803d8aae941d3
willem <willemvdvorst@gmail.com>
2024-01-09 19:26:53 +0100

    hi

--- sources/main.c
+++ sources/main.c
@@ -18,16 +18,16 @@ int main(int argc, char **argv, char **envp)
 	int i = 0;
 	while(1)
 	{
-		line = readline("minishell: ");
-		if (!line)
-			exit(0);
-		// line = "echo hello > \"filename\"";
+		// line = readline("minishell: ");
+		// if (!line)
+		// 	exit(0);
+		line = "echo hello > \"filename\"";
 		if (line[0] == 0)
 		{
 			free(line);
 			continue;
 		}
-		add_history(line);
+		//add_history(line);
 		parsing_return = parsing(&shell, line);
 		if (parsing_return == 0)
 			shexit(&shell, 1);
@@ -37,10 +37,10 @@ int main(int argc, char **argv, char **envp)
 			continue;
 		}
 		shell.first_cmd_copy = shell.cmd_lst;
- 		if(heredoc_parent_prepare(shell.cmd_lst) != 0)
-			shexit(&shell, 1);
-		if(execute_all_cmds(&shell) != 0)
-			shexit(&shell, 1);
+ 		// if(heredoc_parent_prepare(shell.cmd_lst) != 0)
+		// 	shexit(&shell, 1);
+		// if(execute_all_cmds(&shell) != 0)
+		// 	shexit(&shell, 1);
 		clear_all_commands(&shell.first_cmd_copy);
 		shell.first_cmd_copy = NULL;
 		i++;
