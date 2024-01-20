01c044dec9b3df1435d75d002e53545ff121ca1f
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-20 14:39:25 +0100

    shell init norm

--- sources/shell_init/shell_init.c
+++ sources/shell_init/shell_init.c
@@ -16,36 +16,34 @@
 //joining current working directory and program name 
 //return 0 if ok
 // retunr 1 if fail (fail is critical)
-static int set_minishell_exec(char **program)
+static int	set_minishell_exec(char **program)
 {
-	char *path;
+	char	*path;
 
 	path = NULL;
 	path = getcwd(path, 0);
-	if(path == NULL)
+	if (path == NULL)
 	{
 		perror("Cannot set current working directory for path\n");
 		return (EXIT_FAILURE);
 	}
 	*program = ft_strjoin(path, "/minishell");
-	if(*program == NULL)
+	if (*program == NULL)
 	{
 		perror("strjoin in set minishell exec failed\n");
-		return(free(path), EXIT_FAILURE);
+		return (free(path), EXIT_FAILURE);
 	}
-	return(free(path), EXIT_SUCCESS);
+	return (free(path), EXIT_SUCCESS);
 }
 
 //return 1 if failed
 //return 0 if success
 //initialize both headex, and head_env
 //sorts head_ex;
-int shell_init(t_shell *shell, char **envp)
+int	shell_init(t_shell *shell, char **envp)
 {
 	shell->cmd_lst = NULL;
-
 	shell->first_cmd_copy = NULL;
-
 	shell->head_env = NULL;
 	shell->head_ex = NULL;
 	shell->minishell_exec = NULL;
@@ -54,13 +52,13 @@ int shell_init(t_shell *shell, char **envp)
 	shell->stdin_cpy = -1;
 	shell->stdout_cpy = -1;
 	shell->last_exit_code = 0;
-	if(env_list_init(&(shell->head_env), envp) != 1)
-		return(EXIT_FAILURE);
-	if(env_list_init(&(shell->head_ex), envp) != 1)
-		return(EXIT_FAILURE);
+	if (env_list_init(&(shell->head_env), envp) != 1)
+		return (EXIT_FAILURE);
+	if (env_list_init(&(shell->head_ex), envp) != 1)
+		return (EXIT_FAILURE);
 	export("?=0", &shell->head_ex, &shell->head_env, 0);
 	list_sort_alpha(shell->head_ex);
-	if(set_minishell_exec(&(shell->minishell_exec)) != 0)
-		return(EXIT_FAILURE);
-	return(EXIT_SUCCESS);
+	if (set_minishell_exec(&(shell->minishell_exec)) != 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
