c5a91946cfdf18d5287ffbb78833fb776a456f84
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-20 12:31:10 +0100

    export exec norm

--- sources/builtins/export_exec.c
+++ sources/builtins/export_exec.c
@@ -10,29 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"../../headers/minishel.h"
+#include "../../headers/minishel.h"
 
-int export_exec(t_shell *shell, t_cmd *export_cmd)
+int	export_exec(t_shell *shell, t_cmd *export_cmd)
 {
-	int argc;
-	int i;
-	int status;
+	int	argc;
+	int	i;
+	int	status;
 
 	i = 1;
 	argc = get_argc(export_cmd->args);
-	if(argc == 1)
+	if (argc == 1)
 	{
-		status = export(export_cmd->args[1], &shell->head_ex, &shell->head_env, 1);
-		if(status != 0)
+		status = export(export_cmd->args[1],
+				&shell->head_ex, &shell->head_env, 1);
+		if (status != 0)
 			status = 2;
 		return (export_exit_status(status, shell));
 	}
-	while(export_cmd->args[i] != NULL)
+	while (export_cmd->args[i] != NULL)
 	{
-		status = export(export_cmd->args[i], &shell->head_ex, &shell->head_env, 1);
+		status = export(export_cmd->args[i],
+				&shell->head_ex, &shell->head_env, 1);
 		i++;
 	}
-	if(status != 0)
+	if (status != 0)
 		status = 1;
 	return (export_exit_status(status, shell));
 }
