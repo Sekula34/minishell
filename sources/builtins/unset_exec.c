4b982fb593e5d600bd76cb8dc452b49181377716
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-20 12:38:22 +0100

    unset_exec norm

--- sources/builtins/unset_exec.c
+++ sources/builtins/unset_exec.c
@@ -10,23 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../../headers/minishel.h"
 #include "../../headers/data_types.h"
 
-int unset_exec(t_shell *shell, t_cmd *unset_cmd)
+int	unset_exec(t_shell *shell, t_cmd *unset_cmd)
 {
-	int argc;
-	int i;
+	int	argc;
+	int	i;
 
 	i = 1;
 	argc = get_argc(unset_cmd->args);
-	if(argc < 2)
+	if (argc < 2)
 		return (export_exit_status(0, shell));
-	while(unset_cmd->args[i] != NULL)
+	while (unset_cmd->args[i] != NULL)
 	{
 		unset(unset_cmd->args[i], &shell->head_env, &shell->head_ex);
 		i++;
 	}
-	return(export_exit_status(0,shell));
+	return (export_exit_status(0, shell));
 }
