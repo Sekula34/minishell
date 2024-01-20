a5830d189ba87792f7ade1f2d5c870b211c05800
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-20 12:24:12 +0100

    echo.c norm

--- sources/builtins/echo.c
+++ sources/builtins/echo.c
@@ -12,50 +12,6 @@
 
 #include "../../headers/minishel.h"
 
-/* static void echo_print(char *string)
-{
-	int i;
-
-	i = 0;
-	while(string[i] != '\0')
-	{
-		if (string[i] != '\'' && string [i] != '"')
-			ft_putchar_fd(string[i], 1);
-		i++;
-	}
-} */
-
-// static void echo_print(char *string)
-// {
-// 	t_tokens tok;
-// 	int	i;
-
-// 	i = 0;
-// 	init_tok_struct(&tok);
-// 	while (string[i])
-// 	{
-// 		set_quotation(&tok, string[i]);
-// 		if (tok.isq == 0 && tok.idq == 0)
-// 		{
-// 			if (string[i] != '\'' && string [i] != '"')
-// 				ft_putchar_fd(string[i], 1);
-// 		}
-// 		else if (tok.isq == 1)
-// 		{
-// 			if (string[i] != '\'')
-// 				ft_putchar_fd(string[i], 1);
-// 		}
-// 		else if (tok.idq == 1)
-// 		{
-// 			if (string[i] != '"')
-// 				ft_putchar_fd(string[i], 1);
-// 		}
-		
-// 		i++;
-// 	}
-
-// }
-
 //prints string on standard output 
 //if n_option is 1 print without newline 
 //otherwise prints newline
@@ -64,12 +20,10 @@ int	echo(char *string)
 {
 	if (string == NULL)
 	{
-
 	}
 	else
 	{
 		ft_printf("%s", string);
-		//echo_print(string);
 	}
 	return (0);
 }
