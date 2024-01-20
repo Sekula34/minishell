f4edac89011744303b62c73347b9a97324a0a1ea
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-20 14:06:58 +0100

    input red norm

--- sources/execution/redirection/input_redirect.c
+++ sources/execution/redirection/input_redirect.c
@@ -12,25 +12,24 @@
 
 #include "../../../headers/minishel.h"
 
-
 //function that check if file exist and it is readable
 //return 1 if file does not exist or it is not readable
 //print perror in that case 
 //open files and set new_fd to be new filedescritport
 //return 2 if open fails and print perror in that case
 //return 0 on success
-static int file_checker(char *file_name, int *new_fd)
+static int	file_checker(char *file_name, int *new_fd)
 {
 	int		access_val;
 
 	access_val = access(file_name, F_OK | R_OK);
-	if(access_val == -1)
+	if (access_val == -1)
 	{
 		perror("file_checker:");
-		return(1);
+		return (1);
 	}
-	*new_fd = open(file_name, O_RDONLY);
-	if(*new_fd == -1)
+	*new_fd = open (file_name, O_RDONLY);
+	if (*new_fd == -1)
 	{
 		perror("Open in input redirect failed\n");
 		return (2);
@@ -43,19 +42,18 @@ static int file_checker(char *file_name, int *new_fd)
 //retunr 2 if open failed
 //in fd is now standard input
 //fd is not allocated here
-int input_redirect(t_redirect *input, int *fd)
+int	input_redirect(t_redirect *input, int *fd)
 {
-	int result;
+	int	result;
 
 	result = file_checker(input->file_name, fd);
-	if(result != 0)
+	if (result != 0)
 		return (1);
-	if(dup2(*fd, STDIN_FILENO) == -1)
+	if (dup2(*fd, STDIN_FILENO) == -1)
 	{
 		perror("Dup2 in input redirect failed\n");
 		return (1);
 	}
 	close(*fd);
-	return(0);
-
+	return (0);
 }
