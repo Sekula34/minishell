34e31c0b60daf47e0e44be02c524285f4354324c
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-20 14:11:55 +0100

    output redirect

--- sources/execution/redirection/output_redirect.c
+++ sources/execution/redirection/output_redirect.c
@@ -17,16 +17,17 @@
 //return 1 if failes open
 //return 0 if ok
 //file is creted in rdwrite mode
-static int create_file(int *new_fd, char *file_name)
+static int	create_file(int *new_fd, char *file_name)
 {
 	*new_fd = open(file_name, O_RDWR | O_CREAT, 0644);
-	if(*new_fd == -1)
+	if (*new_fd == -1)
 	{
 		perror("Open in create file failed\n");
 		return (1);
 	}
 	return (0);
 }
+
 //function that check if file exist
 //if file does exist check if file have write premission
 //if you have write permision owerwrite file
@@ -35,47 +36,48 @@ static int create_file(int *new_fd, char *file_name)
 //return 0 on success
 //return 1 if no permision
 //retunr 2 if open fails 
-static int file_checker(char *file_name, int *new_fd)
+static int	file_checker(char *file_name, int *new_fd)
 {
 	int		access_val;
+
 	access_val = access(file_name, F_OK);
-	if(access_val == -1)
+	if (access_val == -1)
 	{
-		if(create_file(new_fd, file_name) == 1)
+		if (create_file(new_fd, file_name) == 1)
 		{
-			return(2);
+			return (2);
 		}
 		return (0);
 	}
 	access_val = access(file_name, W_OK);
-	if(access_val == -1)
+	if (access_val == -1)
 	{
 		perror("File checker\n");
 		return (1);
 	}
 	*new_fd = open(file_name, O_TRUNC | O_RDWR);
-	if(*new_fd == -1)
-		return(2);
-	return(0);
+	if (*new_fd == -1)
+		return (2);
+	return (0);
 }
+
 //return 0 on success 
 //return 1 if no such file or dir
 //retunr 2 if open failed
 //in fd is now standard input
 //fd is not allocated here
-int output_redirect(t_redirect *output, int *fd)
+int	output_redirect(t_redirect *output, int *fd)
 {
-	int result;
+	int	result;
 
 	result = file_checker(output->file_name, fd);
-	if(result != 0)
+	if (result != 0)
 		return (1);
-	if(dup2(*fd, STDOUT_FILENO) == -1)
+	if (dup2(*fd, STDOUT_FILENO) == -1)
 	{
 		perror("Dup2 in input redirect failed\n");
 		return (1);
 	}
 	close(*fd);
-	return(0);
+	return (0);
 }
-
