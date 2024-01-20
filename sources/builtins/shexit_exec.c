fceecd54df03fc1560edfaee46683d68d819e59d
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-20 12:42:49 +0100

    shexit norm

--- sources/builtins/shexit_exec.c
+++ sources/builtins/shexit_exec.c
@@ -12,37 +12,38 @@
 
 #include "../../headers/minishel.h"
 
-static int numeric_argument_check(char *str)
+static int	numeric_argument_check(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		while(str[i] == '+' || str[i] == '"' || str[i] == '-')
+		while (str[i] == '+' || str[i] == '"' || str[i] == '-')
 			i++;
-		if(str[i] == '\0')
-			break;
-		if(ft_isdigit(str[i]) != 1)
+		if (str[i] == '\0')
+			break ;
+		if (ft_isdigit(str[i]) != 1)
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-static char* point_to_digit(char *string)
+static char	*point_to_digit(char *string)
 {
-	char *ptr = NULL;
-	int i;
+	char	*ptr;
+	int		i;
 
+	ptr = NULL;
 	i = 0;
-	while(string[i] != '\0')
+	while (string[i] != '\0')
 	{
-		if(string[i] == '-')
-			return(NULL);
-		if(ft_isdigit(string[i]) == 1)
+		if (string[i] == '-')
+			return (NULL);
+		if (ft_isdigit(string[i]) == 1)
 		{
-			ptr=string + i;
+			ptr = string + i;
 			return (ptr);
 		}
 		i++;
@@ -50,12 +51,12 @@ static char* point_to_digit(char *string)
 	return (NULL);
 }
 
-static void set_status(char *exit_argument, int *status)
+static void	set_status(char *exit_argument, int *status)
 {
-	char *digit;
+	char	*digit;
 
 	digit = point_to_digit(exit_argument);
-	if(digit == NULL)
+	if (digit == NULL)
 	{
 		*status = 2;
 		return ;
@@ -64,26 +65,26 @@ static void set_status(char *exit_argument, int *status)
 	return ;
 }
 
-int shexit_exec(t_shell *shell, t_cmd *exit_cmd)
+int	shexit_exec(t_shell *shell, t_cmd *exit_cmd)
 {
-	int argc;
-	int status;
-	char *digit;
+	int		argc;
+	int		status;
+	char	*digit;
 
 	status = 0;
 	//ft_printf("exit\n");
 	argc = get_argc(exit_cmd->args);
-	if(argc > 2)
+	if (argc > 2)
 	{
 		ft_putstr_fd(" too many arguments\n", 2);
 		shell->last_exit_code = export_exit_status(1, shell);
 	}
-	if(argc == 2)
+	if (argc == 2)
 	{
-		if(numeric_argument_check(exit_cmd->args[1]) == 0)
+		if (numeric_argument_check(exit_cmd->args[1]) == 0)
 		{
 			ft_putstr_fd(" numeric argument required\n", 2);
-			shell->last_exit_code= export_exit_status(2, shell);
+			shell->last_exit_code = export_exit_status(2, shell);
 		}
 		set_status(exit_cmd->args[1], &status);
 		export_exit_status(status, shell);
