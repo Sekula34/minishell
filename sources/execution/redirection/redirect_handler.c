4f1546fc0ab2fcbde7a227095571fbaccf2f4cbb
Filip Seles <fseles@c3r7p3.42vienna.com>
2024-01-20 14:15:16 +0100

    redirect handler norm

--- sources/execution/redirection/redirect_handler.c
+++ sources/execution/redirection/redirect_handler.c
@@ -10,36 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"../../../headers/minishel.h"
+#include "../../../headers/minishel.h"
 
 //call function dependingon which redirect is encounterd
 //in_fd, and out_fd are now standard input and output;
-static int redirect_menu(t_redirect *red_list, int *in_fd, int *out_fd, int i)
+static int	redirect_menu(t_redirect *red_list, int *in_fd, int *out_fd, int i)
 {
-	if(red_list->type == 'a')
-		return(append_redirect(red_list, out_fd));
-	if(red_list->type == 'h')
-		return(input_redirect(red_list, in_fd));
-	if(red_list->type == 'i')
-		return(input_redirect(red_list, in_fd));
-	if(red_list->type == 'o')
-		return(output_redirect(red_list, out_fd));
+	if (red_list->type == 'a')
+		return (append_redirect(red_list, out_fd));
+	if (red_list->type == 'h')
+		return (input_redirect(red_list, in_fd));
+	if (red_list->type == 'i')
+		return (input_redirect(red_list, in_fd));
+	if (red_list->type == 'o')
+		return (output_redirect(red_list, out_fd));
 	return (1);
 }
 
 //call all redirection in list
 //0 succesfull
 //1 erorr;
-int redirect_handler(t_redirect *red_list, int *in_fd, int *out_fd)
+int	redirect_handler(t_redirect *red_list, int *in_fd, int *out_fd)
 {
-	int i = 0;
-	if(red_list == NULL)
+	int	i;
+
+	i = 0;
+	if (red_list == NULL)
 	{
 		return (0);
 	}
-	while(red_list != NULL)
+	while (red_list != NULL)
 	{
-		if(redirect_menu(red_list, in_fd, out_fd, i) != 0)
+		if (redirect_menu(red_list, in_fd, out_fd, i) != 0)
 			return (1);
 		red_list = red_list->next;
 		i++;
